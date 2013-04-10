321e3472b65aff76833521bb7f7f9e58f0f91e07
romandashko <romadashko@mail.ru>
2013-04-10 13:29:30 +0300

    v 1.1

--- controllers/tweetscontroller.php
+++ controllers/tweetscontroller.php
@@ -12,13 +12,17 @@ class TweetsController extends Controller
     function getAccessToken()
     {
         $this->set('account', $this->TweetsModel->getAccessToken());
-        //$this->TweetsModel->getAccessToken();
         $this->feed();
         $this->_template->_action = 'feed';
     }
 
     function feed()
     {
+        if(!isset($_SESSION)) session_start();
+        if (!defined("USER_TOKEN")) define("USER_TOKEN", $_SESSION['USER_TOKEN']);
+        if (!defined("USER_SECRET")) define("USER_SECRET", $_SESSION['USER_SECRET']);
+
+        $this->set('account', $_SESSION["account"]);
         $this->set('title', 'Twitter client');
         $this->set('data', $this->TweetsModel->select());
     }
@@ -32,7 +36,6 @@ class TweetsController extends Controller
     {
         if (array_key_exists('msg', $_POST) && isset($_POST['msg'])) {
             $this->set('account', $this->TweetsModel->postTweet($_POST['msg']));
-            //$this->TweetsModel->postTweet($_POST['msg']);
             $this->feed();
             $this->_template->_action = 'feed';
         }
